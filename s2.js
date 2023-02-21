d1b727a9658da5beb87ee0b6223d0ee953ad2dea
Eric Starks <123973291+EricDStarks@users.noreply.github.com>
2023-02-20 21:55:56 -0600

    index

---
+++ s2.js
@@ -0,0 +1,10 @@
+function yes() {
+    let ex1 = "Enter name"
+    let text;
+    let person = prompt("Enter name here:");
+    {
+        text = "I love seafood too " + person + "!";
+    }
+    document.getElementsByClassName("ex1"),innerHTML = text;
+    
+}
